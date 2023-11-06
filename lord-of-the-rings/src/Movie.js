06c879bb0c21b6a06374b11cf3f2eeffa2cc8e11
mHaines331 <mhaines9219@gmail.com>
2023-11-06 13:25:15 -0800

    add stylesheet

--- lord-of-the-rings/src/Movie.js
+++ lord-of-the-rings/src/Movie.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Movie extends Component {
   render() {
     return (
-      // we'll add JSX here
-    )
+      <>
+        <h1>The Lord of the Rings: A Trilogy</h1>
+        <p>9h 18min</p>
+      </>
+    );
   }
 }
 
-export default Movie
+export default Movie;
